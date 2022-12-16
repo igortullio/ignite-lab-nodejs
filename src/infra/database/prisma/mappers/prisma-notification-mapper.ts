73cbd5166d7e94c2fdfe77df16c6a20c1427b725
Igor Tullio <igortullio@hotmail.com>
2022-12-15 21:47:36 -0300

    Implement prisma repository

--- src/infra/database/prisma/mappers/prisma-notification-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -1,4 +1,6 @@
+import { Notification as RawNotificaation } from '@prisma/client';
 import { Notification } from '@app/entities/notification';
+import { Content } from '@app/entities/content';
 
 export class PrismaNotificationMapper {
   static toPrisma(notification: Notification) {
@@ -10,5 +12,19 @@ export class PrismaNotificationMapper {
       readAt: notification.readAt,
       createdAt: notification.createdAt,
     };
+  }
+
+  static toDomain(raw: RawNotificaation): Notification {
+    return new Notification(
+      {
+        content: new Content(raw.content),
+        category: raw.category,
+        recipientId: raw.recipientId,
+        readAt: raw.readAt,
+        canceledAt: raw.canceledAt,
+        createdAt: raw.createdAt,
+      },
+      raw.id,
+    );
   }
 }
